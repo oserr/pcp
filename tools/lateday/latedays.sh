55895ede58143179ad366953a22c9e5f5f69b608
Omar Serrano <oser520@gmail.com>
2018-05-03 22:22:14 -0500

    Add logic to submit each job separately

--- tools/lateday/latedays.sh
+++ tools/lateday/latedays.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-# Limit execution time to 20 minutes
-#PBS -lwalltime=0:40:00
-# Allocate all available CPUs on a single node
-#PBS -l nodes=1:ppn=24
+# Submits one job per script.
 
-# Go to the directory from which you submitted your job
-cd $PBS_O_WORKDIR
-
-export LD_LIBRARY_PATH=/opt/gcc/4.9.2/lib64:/opt/gcc/4.9.2/lib:${LD_LIBRARY_PATH}
-
-# Insert Heavy
-./build/src/benchmark -n 1000000 --inserts 0.8 --removals 0.1 --lookups 0.1 --preload 0.2
-./build/src/benchmark -n 1000000 --inserts 0.5 --removals 0.25 --lookups 0.25 --preload 0.5
-
-# Lookup heavy
-./build/src/benchmark -n 1000000 --inserts 0.1 --removals 0.1 --lookups 0.8 --preload 0.9
-./build/src/benchmark -n 1000000 --inserts 0.25 --removals 0.25 --lookups 0.5 --preload 0.75
-
-# Removal heavy
-./build/src/benchmark -n 1000000 --inserts 0.1 --removals 0.8 --lookups 0.1 --preload 0.9
-./build/src/benchmark -n 1000000 --inserts 0.25 --removals 0.5 --lookups 0.25 --preload 0.75
+for op in insert lookup removal
+do
+    for load in heavy medium
+    do
+        qsub -q timer ${op}_${load}.sh
+    done
+done
