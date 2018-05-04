b88053e3a859104838e4d6237b697c3c5be0cafb
Omar Serrano <oser520@gmail.com>
2018-05-03 22:32:17 -0500

    Specify full path from lateday dir

--- tools/lateday/insert_medium.sh
+++ tools/lateday/insert_medium.sh
@@ -4,16 +4,18 @@
 # Allocate all available CPUs on a single node
 #PBS -l nodes=1:ppn=24
 
+# Definitions
+export LD_LIBRARY_PATH=/opt/gcc/4.9.2/lib64:/opt/gcc/4.9.2/lib:${LD_LIBRARY_PATH}
+program=../../build/src/benchmark
+
 # Go to the directory from which you submitted your job
 cd $PBS_O_WORKDIR
-
-export LD_LIBRARY_PATH=/opt/gcc/4.9.2/lib64:/opt/gcc/4.9.2/lib:${LD_LIBRARY_PATH}
 
 date_now=$(date)
 echo "##### Job started at: ${date_now} #####"
 echo
 
-./build/src/benchmark -n 1000000 --inserts 0.5 --removals 0.25 --lookups 0.25 --preload 0.5
+${program} -n 1000000 --inserts 0.5 --removals 0.25 --lookups 0.25 --preload 0.5
 
 date_now=$(date)
 echo
--- tools/lateday/lookup_heavy.sh
+++ tools/lateday/lookup_heavy.sh
@@ -4,16 +4,18 @@
 # Allocate all available CPUs on a single node
 #PBS -l nodes=1:ppn=24
 
+# Definitions
+export LD_LIBRARY_PATH=/opt/gcc/4.9.2/lib64:/opt/gcc/4.9.2/lib:${LD_LIBRARY_PATH}
+program=../../build/src/benchmark
+
 # Go to the directory from which you submitted your job
 cd $PBS_O_WORKDIR
-
-export LD_LIBRARY_PATH=/opt/gcc/4.9.2/lib64:/opt/gcc/4.9.2/lib:${LD_LIBRARY_PATH}
 
 date_now=$(date)
 echo "##### Job started at: ${date_now} #####"
 echo
 
-./build/src/benchmark -n 1000000 --inserts 0.1 --removals 0.1 --lookups 0.8 --preload 0.9
+${program} -n 1000000 --inserts 0.1 --removals 0.1 --lookups 0.8 --preload 0.9
 
 date_now=$(date)
 echo
