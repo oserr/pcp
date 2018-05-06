86323ee73709df8b7979b5418d55080b3b1b1da0
patricio <patriciochilano@gmail.com>
2018-05-06 05:54:40 -0400

    format

--- src/benchmark_runner.h
+++ src/benchmark_runner.h
@@ -229,7 +229,7 @@ RunnerResults BenchmarkRunner::RunMapSingle(const std::string &mapName) {
   using namespace std::chrono;
   using MapType = TMap<int, int>;
   RunnerResults results(mapName, params);
-  MapType hashMap( (int) (params.n / params.mapLoadFactor) );
+  MapType hashMap((int)(params.n / params.mapLoadFactor));
   auto buffers = PreloadMap(hashMap, 1);
   assert(buffers.size() == 1);
   auto timeStart = steady_clock::now();
@@ -248,8 +248,8 @@ RunnerResults BenchmarkRunner::RunMap(const std::string &mapName) {
   std::thread threads[params.maxThreads];
   for (size_t c = params.minThreads; c <= params.maxThreads; ++c) {
     MapType hashMap(params.scalingMode == ScalingMode::Problem
-                        ? (int) (params.n / params.mapLoadFactor)
-                        : (int) ((params.n * c) / params.mapLoadFactor));
+                        ? (int)(params.n / params.mapLoadFactor)
+                        : (int)((params.n * c) / params.mapLoadFactor));
     auto buffers = PreloadMap(hashMap, c);
     auto timeStart = steady_clock::now();
     for (size_t t = 1; t < c; ++t) {
